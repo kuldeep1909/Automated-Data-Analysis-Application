30b6c8440a56c6829471bbc9d23e4483f2ec0864
Kuldeep Malviya <157812775+kuldeep1909@users.noreply.github.com>
2024-08-30 17:16:20 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -16,9 +16,9 @@ st.subheader(':gray[Explore Data with ease]', divider="rainbow")
 file = st.file_uploader("Please drop your csv or excel file", type=['csv', 'xlsx'])
 if file != None:
     if file.name.endswith ('csv'):
-        data = pd.read_csv(file)
+        data = pd.read_csv(file,  encoding='utf-8', errors = 'ignore')
     else:
-        data = pd.read_excel(file)
+        data = pd.read_excel(file, engine= 'python', encoading = 'latin, errors='ignore')
 
     st.dataframe(data)
     st.info("File is successfully uploaded", icon="🚨")
